aa96fc201f69e2b72727b2229d764bc2605b0df3
zacklukinov <zacklukinov@gmail.com>
2022-12-25 12:58:27 +0300

    Add exercise 13

--- Exercise_13/Program.cs
+++ Exercise_13/Program.cs
@@ -2,3 +2,34 @@
 //645 -> 5
 //78 -> третьей цифры нет
 //32679 -> 6
+//Решается через  цикл + надо учесть отрицательные значения
+
+int num_3lgt = 0;
+while (num_3lgt.ToString().Length < 3)
+{
+    Console.WriteLine("Введите трёхзначное число: ");
+    try
+    {
+        num_3lgt = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+    }
+    catch (System.FormatException)
+    {
+        Console.WriteLine("Конец программы!");
+        break;
+    }
+    if (num_3lgt.ToString().Length < 3)
+    {
+        Console.WriteLine("Число не трёхзначное!");
+    }
+    else
+    {
+        int num_3 = 0;
+        while (num_3lgt.ToString().Length >= 3)
+        {
+            num_3 = num_3lgt % 10;
+            num_3lgt = num_3lgt / 10;
+        }
+        Console.WriteLine($"Третья цифра в числе ==> {num_3}");
+        break;
+    }
+}
