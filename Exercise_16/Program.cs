ebf87714ecfbd770fc1c13e1295bc73f8f78cb82
zacklukinov <zacklukinov@gmail.com>
2022-12-25 13:40:54 +0300

    Add exercise 16

--- Exercise_16/Program.cs
+++ Exercise_16/Program.cs
@@ -2,3 +2,19 @@
 //6 -> да
 //7 -> да
 //1 -> нет
+
+Console.WriteLine("Введите число от 1 до 7, соответствующее дню недели: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1 || number > 7)
+{
+    Console.WriteLine ("Неправильный ввод");
+} 
+if (number/6 == 1)
+{
+    Console.WriteLine("Выходной!");
+}
+else
+{
+Console.WriteLine("Рабочий день");
+} 
